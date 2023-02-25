17cc2fd754bcfc459d9edc558f6fd6b381fb8889
Daniel Holmberg <daniel.holmberg97@gmail.com>
2023-02-25 14:53:08 +0200

    Create full sized slurm jobs

--- submit_plotting.sh
+++ submit_plotting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --partition=test
-#SBATCH --time=00:05:00
+#SBATCH --partition=small
+#SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
 
--- submit_training.sh
+++ submit_training.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --partition=test
-#SBATCH --time=00:05:00
-#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:v100:1
 
 
 print_usage() {
@@ -26,4 +27,4 @@ done
 module load pytorch/1.13
 pip install matplotlib tqdm gif scikit-learn ptflops
 
-train.py -i $(pwd)/sample/data -o $(pwd)/$directory/$SLURM_JOB_NAME --data-splits 0.6 0.2 0.2 --epochs 5 $feature_flags $preprocessing_flag --kernel-size $kernel_size
+train.py -i $(pwd)/data -o $(pwd)/$directory/$SLURM_JOB_NAME --gpus 0 --num-workers 6 --epochs 10000 --batch-size 10 $feature_flags $preprocessing_flag --kernel-size $kernel_size
